b3789f2816373b1af76c7068a5cf1f66d59b06a5
archae0pteryx <>
2017-08-04 09:10:38 -0700

    auto kick

--- stripper.sh
+++ stripper.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-# Need to add option for inline comments
+# @archae0pteryx
+# add option for inline comments
+# add option for whole directories
+# add option for other types of comments
+
 sed -E '/^[[:blank:]]*(\/\/|#)/d;s/#.*//' $1 > "stripped.${1}"
